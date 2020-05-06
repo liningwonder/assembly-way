ab29ae09974869c43b5a0268128e1e07b49da02c
liningwonder <liningwonder@163.com>
2020-05-06 15:00:08 +0800

    modify

--- src/main/java/com/datafusion/maven/assembly/Main.java
+++ src/main/java/com/datafusion/maven/assembly/Main.java
@@ -11,8 +11,10 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-
+        //配置JVM参数 -Dlog4j.configurationFile=D:\\assembly-tar\\conf\\log4j2.xml
+        String path = System.getProperty("log4j.configurationFile", " ");
         MDC.put("REQUEST_ID", UUID.randomUUID().toString());
+        LOG.warn("the default log4j2 configuration file is " + path);
         EchoServer.main(args);
     }
 
